29c9495484c1077d63c6f82d51178cec3067c35f
Fatima Siddique <fatimasidq@gmail.com>
2021-12-03 00:44:18 -0500

    added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,8 @@ const request = require("request");
 const fetchBreedDescription = function(breed, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, _response, body) => {
     body = JSON.parse(body);
-    return callback(error, body[0].description);
+    error = body.length < 1 ? "Invalid breed!" : error;
+    return callback(error, error ? null : body[0].description);
   });
 };
 
